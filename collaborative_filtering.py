98282dc3551f8bd46801cf07279774e7bcf98e47
jakubkarczewski <jakub.karczewski@gmail.com>
2019-02-02 11:48:05 +0100

    Add changes to baselines

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -1,19 +1,18 @@
 from os.path import isfile, join, isdir
 from os import getcwd, mkdir
-
 from time import time
+from math import ceil
 
 import pandas as pd
 import numpy as np
 from datasketch import MinHash, MinHashLSHForest
 
-# todo: can we use wminhash somehow?
+
 # todo: refactor for style
 # todo: fix oop
 # todo: cache based on hash value of input files
-# todo: base class + recommender + lsh recommender (look at notebook)
 class BaseCFRecommender:
-    """Recommender prototype, works on small data only."""
+    """Base class for collaborative filtering tasks."""
     def __init__(self, ratings_path, titles_path):
         assert isfile(titles_path)
         self._titles_path = titles_path
@@ -50,10 +49,11 @@ class BaseCFRecommender:
 
     @classmethod
     def _get_sim_candidates(cls, user_ratings, corr_matrix):
+        """Computes similarity score for each item in dataset based on user's ratings."""
         sim_candidates = pd.Series()
 
         for i in range(len(user_ratings.index)):
-            # get similar movies
+            # get similar items
             sims = corr_matrix[user_ratings.index[i]].dropna()
             # scale by rating
             sims = sims.map(lambda x: x * user_ratings[i])
@@ -62,27 +62,31 @@ class BaseCFRecommender:
 
         sim_candidates = sim_candidates.groupby(sim_candidates.index).sum()
         sim_candidates.sort_values(inplace=True, ascending=False)
-        try:
-            sim_candidates.drop(user_ratings.index)
-        except KeyError:
-            pass
+        sim_candidates_list = list(sim_candidates.index)
 
-        return sim_candidates
+        for title in user_ratings.index:
+            try:
+                sim_candidates_list.remove(title)
+            except Exception:
+                print('unable to remove', title)
+                pass
 
-    def get_recommendation(self):
+        return sim_candidates_list
+
+    def get_recommendation(self, top_n=None):
         raise NotImplementedError
 
 
 class CFRecommender(BaseCFRecommender):
     def __init__(self, ratings_path, titles_path, user_ratings_path):
         super(CFRecommender, self).__init__(ratings_path, titles_path)
-
         assert isfile(user_ratings_path)
         self._user_ratings_path = user_ratings_path
         self._sim_dir = None
 
-    def get_recommendation(self, top_n=10):
-        """Get top N recommendations knowing user ratings."""
+    def get_recommendation(self, top_n=None):
+        """Get top N recommendations knowing user's ratings."""
+        start = time()
         self._users_ratings = self._parse_dataframes()
         try:
             self._corr_matrix = self._get_corr(self._users_ratings)
@@ -92,7 +96,9 @@ class CFRecommender(BaseCFRecommender):
         user_ratings = self._load_user_ratings(self._user_ratings_path)
         candidates = self._get_sim_candidates(user_ratings, self._corr_matrix)
 
-        return list(candidates.index)[:top_n]
+        print('Recommendation took {} seconds'.format(str(time() - start)))
+
+        return candidates[:top_n]
 
     def generate_dataset(self, show_mem_usage=False):
         """Save user-item rating matrix and correlation matrix"""
@@ -131,11 +137,11 @@ class CFLSHRecommender(BaseCFRecommender):
         self._permutations = permutations
         self._forest = None
         self._id_title_map = None
+        self._user_profile = None
 
     def _filter_negative(self):
         self._users_ratings = self._parse_dataframes()
         self._users_ratings = self._users_ratings.applymap(lambda x: np.nan if x < 3 else x - 2)
-
 
     @staticmethod
     def _to_shingle(column):
@@ -146,11 +152,9 @@ class CFLSHRecommender(BaseCFRecommender):
                 shingle.append(i)
         return shingle
 
-    def get_forest(self):
-        start_time = time()
-
+    def _get_forest(self):
+        # start_time = time()
         minhash = []
-
         self._filter_negative()
 
         for movie_name in self._users_ratings.columns:
@@ -162,73 +166,74 @@ class CFLSHRecommender(BaseCFRecommender):
 
         forest = MinHashLSHForest(num_perm=self._permutations)
 
-        for i,m in enumerate(minhash):
-            forest.add(i,m)
+        for i, m in enumerate(minhash):
+            forest.add(i, m)
 
         assert len(minhash) == len(self._users_ratings.columns), "Sanity check failed"
         self._id_title_map = {i: title for i, title in enumerate(self._users_ratings.columns)}
 
         forest.index()
 
-        print('It took %s seconds to build forest.' % (time()-start_time))
+        # print('It took %s seconds to build forest.' % (time()-start_time))
 
         self._forest = forest
 
         return forest
 
-    def get_recommendation(self, num_results=10):
+    def get_recommendation(self, top_n=None):
+
         start_time = time()
 
-        user_profile = self._load_user_ratings(self._user_ratings_path)
-        user_profile_positive = user_profile[user_profile >= 3]
+        if not self._forest:
+            self._get_forest()
+
+        self._user_profile = self._load_user_ratings(self._user_ratings_path)
+        user_profile_positive = self._user_profile[self._user_profile >= 3]
 
         liked_titles = list(user_profile_positive.index)
 
         arrays = []
         for liked_movie in liked_titles:
-            print("liked: ", liked_movie)
+            # print("liked: ", liked_movie)
             tokens = self._to_shingle(self._users_ratings[liked_movie])
             m = MinHash(num_perm=self._permutations)
 
             for token in tokens:
                 m.update(str(token).encode('utf8'))
 
-            idx_array = np.array(self._forest.query(m, num_results))
+            idx_array = np.array(self._forest.query(m, top_n))
             if len(idx_array) == 0:
-                return None  # if your query is empty, return none
+                return None
 
-                #     result = database.iloc[idx_array]['title']
-
-            print('It took %s seconds to query forest.' % (time() - start_time))
+            # print('It took %s seconds to query forest.' % (time() - start_time))
             arrays.append(idx_array)
 
-        recs = []
-        for subarray in arrays:
+        recs = {title: None for title in liked_titles}
+        for subarray, title in zip(arrays, recs.keys()):
             subrecs = []
             for elem in subarray:
                 try:
-                    subrecs.append(self._id_title_map[elem])
+                    if self._id_title_map[elem] not in recs.keys():
+                        subrecs.append(self._id_title_map[elem])
                 except KeyError:
-                    subrecs.append("Keyerror")
-            recs.append(subrecs)
+                    pass
+            recs[title] = subrecs[:top_n]
+
+        print('It took %s seconds to get recommendation.' % (time()-start_time))
         return recs
 
+    def reduce_to_n(self, recs, top_n):
+        """Reduce recommendations based on user's ratings value. Movies recommended for titles which user liked most
+        will have more positions in the final list.
+        """
+        # compute how many places in final list each movie has
+        sum_ratings = sum([value for index, value in self._user_profile.iteritems()])
 
+        num_positions = {title: ceil((self._user_profile[title]/sum_ratings) * top_n) for title in recs.keys()}
 
-if __name__ == '__main__':
-    # recommender = CFRecommender("../ratings_relevant_medium.csv",
-    #                             "../titles.csv",
-    #                             "./voting_template_filled.csv")
-    #
-    # #print(recommender.get_recommendation())
-    #
-    # recommender.generate_dataset(show_mem_usage=True)
+        final_list = []
+        for title in recs.keys():
+            for i in range(num_positions[title]):
+                final_list.append(recs[title][i])
 
-    recommender = CFLSHRecommender("../ratings_relevant_medium.csv",
-                                   "../titles.csv",
-                                   "./voting_template_filled.csv",
-                                   128)
-    recommender.get_forest()
-    for elem in recommender.get_recommendation():
-        print(elem)
-
+        return final_list[:top_n]
